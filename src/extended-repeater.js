bae1f767bacbaf6194f383724e384721a3909bf7
folent <folent1@yandex.ru>
2020-03-22 18:21:29 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,40 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    let result = [];
+
+    if (typeof options.repeatTimes == 'undefined') {
+
+        options.repeatTimes = 1;
+
+    }
+
+    if ('addition' in options) {
+
+        options.addition += '';
+
+        if ('additionRepeatTimes' in options || 'additionSeparator' in options) {
+
+            options.addition = repeater(options.addition, { repeatTimes: options.additionRepeatTimes, separator: options.additionSeparator });
+            str += options.addition;
+
+        }
+    }
+    for (let i = 0; i < options.repeatTimes; i++) {
+
+        result.push(str);
+    }
+    if ('separator' in options || typeof options.separator !== 'undefined') {
+
+        sep = options.separator;
+
+    } else {
+
+        sep = '+';
+    }
+    return result.join(sep);
+
+
+
+
+
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,79 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(mode = true) {
+
+        this.mode = mode;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+
+    encrypt(message, key) {
+
+        if (!message || !key || message.length <= 0 || key.length <= 0) {
+
+            new Error;
+
+        }
+        let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        let result = [];
+        let j = 0;
+
+        message = message.toUpperCase();
+        key = key.repeat(Math.ceil(message.length / key.length)).toUpperCase();
+
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].match(/[a-z]/gi)) {
+                if (alphabet.indexOf(message[i]) + alphabet.indexOf(key[j]) > 25) {
+
+                    result.push(alphabet[alphabet.indexOf(message[i]) + alphabet.indexOf(key[j]) - 26]);
+                } else {
+
+                    result.push(alphabet[alphabet.indexOf(message[i]) + alphabet.indexOf(key[j])]);
+                }
+                j++;
+            } else {
+
+                result.push(message[i]);
+
+            }
+
+        }
+        return this.mode ? result.join('') : result.reverse().join('');
+
     }
+
+    decrypt(ecryptedMessage, key) {
+        if (!ecryptedMessage || !key || ecryptedMessage.length <= 0 || key.length <= 0) {
+            throw Error;
+        }
+
+        let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        let result = [];
+        let j = 0;
+        key = key.repeat(Math.ceil(ecryptedMessage.length / key.length)).toUpperCase();
+
+        for (let i = 0; i < ecryptedMessage.length; i++) {
+
+            if (ecryptedMessage[i].match(/[a-z]/gi)) {
+
+                if (alphabet.indexOf(ecryptedMessage[i]) - alphabet.indexOf(key[j]) < 0) {
+
+                    result.push(alphabet[alphabet.indexOf(ecryptedMessage[i]) - alphabet.indexOf(key[j]) + 26]);
+
+                } else {
+
+                    result.push(alphabet[alphabet.indexOf(ecryptedMessage[i]) - alphabet.indexOf(key[j])]);
+                }
+                j++;
+            } else {
+
+                result.push(ecryptedMessage[i]);
+            }
+        }
+        return this.mode ? result.join('') : result.reverse().join('');
+
+    }
+
+
 }
 
 module.exports = VigenereCipheringMachine;
